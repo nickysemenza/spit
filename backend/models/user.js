c50b4d7bce5e30eb6f64bf41d3e6fd2e497a3177
nickschneider <schnei54@purdue.edu>
2017-02-07 18:32:10 -0500

    I added the game.js file to the models folder. It holds the schema for the game entry into the Database. Additionally, I added onto the schema for the user entry into the database

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,7 +3,10 @@ let mongoose = require('mongoose')
   ,Schema = mongoose.Schema;
 
 let userSchema = new Schema({
-  username: {type: String, required: true, unique: true}
-});
+        username: {type: String, required: true, unique: true},
+        password: {type: String, required: true},
+        gamesPlayer: {type: Number, required: true},
+        gamesWon: {type: Number, required: true}
+    });
 
 module.exports = mongoose.model('User', userSchema);
