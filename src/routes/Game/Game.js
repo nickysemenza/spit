ec52d1b158e9370ecce05f2932dbf180d4a93a36
Nicky Semenza <nicky@nickysemenza.com>
2017-02-07 12:34:24 -0500

    key handler for numbers and spacebar

--- src/routes/Game/Game.js
+++ src/routes/Game/Game.js
@@ -2,16 +2,20 @@ import React, { Component, PropTypes } from 'react';
 import { Grid, Row, Col, Panel } from 'react-bootstrap';
 import {SOCKET_ADDRESS} from '../../config';
 import Card from './Card';
+import KeyHandler, {KEYPRESS} from 'react-key-handler';
 
 export default class Game extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      moveBox: ""
+      moveBox: "",
+      selectedHand: null
     };
     this.handleMoveBoxChange = this.handleMoveBoxChange.bind(this);
     this.sendMoveDebug = this.sendMoveDebug.bind(this);
     this.startGame = this.startGame.bind(this);
+    this.changeSelectedHand = this.changeSelectedHand.bind(this);
+    this.popDeck = this.popDeck.bind(this);
   }
   handleMoveBoxChange(event) {
     this.setState({moveBox: event.target.value});
@@ -45,16 +49,33 @@ export default class Game extends Component {
       this.websocket.send('AUTH '+this.props.user.token);
     };
   }
+  keyPressed (key) {
+    console.log("KEYPRESS: "+key);
+  }
+  changeSelectedHand(ind) {
+    this.setState({selectedHand: ind});
+  }
+  popDeck() {
+    this.sendMove("MOVE POP-DECK");
+  }
+
   render () {
     return (<div>
         <h1>Game #{this.props.game_id}</h1>
         <h2>Hello, {this.props.user.username}</h2>
+        <KeyHandler keyEventName={KEYPRESS} keyValue="1" onKeyHandle={()=>{this.changeSelectedHand(1)}} />
+        <KeyHandler keyEventName={KEYPRESS} keyValue="2" onKeyHandle={()=>{this.changeSelectedHand(2)}} />
+        <KeyHandler keyEventName={KEYPRESS} keyValue="3" onKeyHandle={()=>{this.changeSelectedHand(3)}} />
+        <KeyHandler keyEventName={KEYPRESS} keyValue="4" onKeyHandle={()=>{this.changeSelectedHand(4)}} />
+        <KeyHandler keyEventName={KEYPRESS} keyValue=" " onKeyHandle={this.popDeck} />
+
 
         <input type="text" value={this.state.moveBox} onChange={this.handleMoveBoxChange} />
         <button onClick={this.sendMoveDebug}>send move</button>
         <button onClick={this.startGame}>start game</button>
 
-        <Card type={1} />
+        {/*<Card type={1} />*/}
+        <pre>Currently selected hand index (from numkeys): {this.state.selectedHand}</pre>
         <pre>{JSON.stringify(this.props.game.state, null, 2)}</pre>
     </div>
     );
