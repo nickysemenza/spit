40c529f894d9f576c352d16f3c4385a626ae4fc4
nickschneider <schnei54@purdue.edu>
2017-02-16 13:30:51 -0500

    The Leaderboard.js file now sends the data from the database to the whoever calls that function. It is currently in the format of { username: ,totalScore: , gamesWon: }

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -5,34 +5,12 @@ let user = require('../models/user.js');
 
 
 router.get('/', (req, res) => {
-  user.find({ }).select('username totalScore').exec(function (err, users) {
+  user.find({ }).select('username totalScore gamesWon').exec(function (err, users) {
     if (err) {
       console.log(err);
     }
-    console.log(users);// Space Ghost is a talk show host.
+    res.send(users);
   });
-  res.json([
-    {
-      username: "marty17",
-      win: 432,
-      loss: 32
-    },
-    {
-      username: "marty1234",
-      win: 445,
-      loss: 31
-    },
-    {
-      username: "marty2134fakedata",
-      win: 41,
-      loss: 3
-    },
-    {
-      username: "nicky",
-      win: 42,
-      loss: 13
-    }
-  ]);
 });
 
 module.exports = router;
