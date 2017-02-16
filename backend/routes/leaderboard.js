318297beeb010d5376f27f189900d111da6e1f76
nickschneider <schnei54@purdue.edu>
2017-02-16 12:29:00 -0500

    trying to get the user information from the DB to print to the log

--- backend/routes/leaderboard.js
+++ backend/routes/leaderboard.js
@@ -1,12 +1,17 @@
 let express = require('express');
 let router  = express.Router();
 let mongoose = require('mongoose');
+let user = require('../models/user.js')
 
 
 router.get('/', (req, res) => {
+  /*user.find({ }).exec(function (err, users) {
+    if (err) return handleError(err);
+    console.log(users);// Space Ghost is a talk show host.
+  });*/
   res.json([
     {
-      username: "marty",
+      username: "marty19010",
       win: 432,
       loss: 32
     },
