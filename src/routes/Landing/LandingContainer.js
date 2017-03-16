cb71ffd38445c6ee78ec75eb2a8ed4232ac8c0a4
Nicky Semenza <nicky@nickysemenza.com>
2017-03-15 21:45:15 -0500

    fixed shortid dependency issue

--- src/routes/Landing/LandingContainer.js
+++ src/routes/Landing/LandingContainer.js
@@ -3,7 +3,7 @@ import { fetchLeaderboard } from '../../actions/leaderboardActions';
 import { createUser, logout } from '../../actions/userActions';
 import { joinLobbyGame } from '../../actions/gameActions';
 import Landing from './Landing';
-var shortid = require('shortid');
+import  shortid from 'shortid';
 
 function mapStateToProps (state) {
   return {
