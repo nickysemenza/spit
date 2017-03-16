613386b281de5babb268a3f044d72bb3a68ba6a6
nschneider18 <schnei54@purdue.edu>
2017-03-16 12:20:06 -0400

    Saving a game after it has been completed. Also updating the total number of wins
    Lastly I added an extra field called mongoID to the client. Eventually
    this will be phased in as the the primary identifier versus the
    username. The reason for this is because usernames can changes over time
    whereas the mongoID will not

--- backend/bin/Game.js
+++ backend/bin/Game.js
@@ -40,20 +40,22 @@ class Game {
     //save this.gameState, this.id, etc
     let gameJSON = new gameSchema({
       id: this.id,
-      url: this.url,
-      players: this.clients.names,
       totalMoves: this.stateSnapshots.length,
-      winner: this.winner,
+      winner: this.winner[0],
       state: this.stateSnapshots
+    });
+
+    this.clients.forEach((c)=>{
+      gameJSON.players.push(c.name);
     });
 
     gameJSON.save((err, g) => {
       if (err) return console.error(err);
-    });
+   });    
   }
 
   updateWinner(){
-    User.findOneAndUpdate({'username': 'Nicholas'}, {$inc: { gamesWon: 1} }, {upsert:true}, function(err, doc){
+    User.findOneAndUpdate({'username': this.winner[0]}, {$inc: { gamesWon: 1} }, {upsert:true}, function(err, doc){
       if (err) return console.log(500, { error: err });
       return console.log("succesfully saved");
     });
@@ -201,8 +203,8 @@ class Game {
     }
 
     this.saveGame();
-    this.updateWinner();
     this.updateUsers();
+    this.updateWinner();
     console.log(this.winner);
 
   }
@@ -388,7 +390,7 @@ class Game {
     // console.log(this.gameState.decks);
   }
   getShuffledDeck() {
-    let cards = [...Array(52).keys()].map(x => ++x);
+    let cards = [...Array(10).keys()].map(x => ++x);
     return this.shuffle(cards.slice(0));
   }
   getGameState(username) {
